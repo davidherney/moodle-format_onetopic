bb31fad6d5823a1bb065c9756cf3efa11ca8317c
cirano <davidherney@gmail.com>
2019-12-16 03:20:57 -0500

    Fix pointer-events restriction for help button

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018010607;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2018010608;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'format_onetopic';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
