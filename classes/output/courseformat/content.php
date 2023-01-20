a646e961f4bcf8901968055ddc046077c6f6a261
David Herney <davidherney@gmail.com>
2023-01-20 18:16:11 -0500

    Message when edit and the tabs are hidden

--- classes/output/courseformat/content.php
+++ classes/output/courseformat/content.php
@@ -109,7 +109,8 @@ class content extends content_base {
             'secondrow' => $secondtabslist,
             'tabsviewclass' => $tabsview,
             'hasformatmsgs' => count(\format_onetopic::$formatmsgs) > 0,
-            'formatmsgs' => \format_onetopic::$formatmsgs
+            'formatmsgs' => \format_onetopic::$formatmsgs,
+            'hidetabsbar' => ($course->hidetabsbar == 1 && $format->show_editor())
         ];
 
         // The current section format has extra navigation.
