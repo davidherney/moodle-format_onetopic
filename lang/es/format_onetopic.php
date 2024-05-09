45f21f6fea84fe1dd9754ca4ca8adeaa62c84012
David Herney <davidherney@gmail.com>
2024-05-09 00:15:53 -0500

    Code compliance

--- lang/es/format_onetopic.php
+++ lang/es/format_onetopic.php
@@ -23,94 +23,83 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['currentsection'] = 'Este tema';
-$string['pluginname'] = 'Temas en pestañas';
-$string['sectionname'] = 'Tema';
-$string['page-course-view-topics'] = 'Alguna página principal de curso en formato onetopic';
-$string['page-course-view-topics-x'] = 'Alguna página de curso en formato onetopic';
-$string['hidefromothers'] = 'Ocultar tema';
-$string['showfromothers'] = 'Mostrar tema';
-$string['hidetabsbar'] = 'Ocultar barra de pestañas';
-$string['hidetabsbar_help'] = 'Oculta la barra de pestañas en la página principal del curso, la navegación se llevará a cabo con la barra de navegación entre temas.';
-
-$string['movesectionto'] = 'Mover el tema actual';
-$string['movesectionto_help'] = 'Mover el tema actual <strong>antes</strong> de (para temas a la izquierda del actual) o <strong>después</strong> de (para temas a la derecha del actual) el tema que seleccione';
-
-$string['utilities'] = 'Utilidades de edición de pestañas';
-$string['disableajax'] = 'Acciones de edición asíncronas';
-$string['disable'] = 'Deshabilitar';
-$string['enable'] = 'Habilitar';
-$string['disableajax_help'] = 'Deshabilitarlas le permite mover recursos entre pestañas de temas. Sólo se deshabilitan las acciones asíncronas en la sesión actual, no es permanente.';
-
-$string['subtopictoright'] = 'Mover a la derecha como pestaña hija';
-
-$string['duplicatesection'] = 'Duplicar tema actual';
-$string['duplicatesection_help'] = 'Usado para duplicar los recursos del tema actual en un nuevo tema.';
-$string['duplicate'] = 'Duplicar';
-$string['duplicating'] = 'Duplicando';
-$string['creating_section'] = 'Creando el nuevo tema';
-$string['rebuild_course_cache'] = 'Recreando el caché del curso';
-$string['duplicate_confirm'] = '¿Está seguro de que desea duplicar el tema actual? La tarea puede tardar un buen rato dependiendo de la cantidad de recursos.';
-$string['cantcreatesection'] = 'Error creando un nuevo tema';
-$string['progress_counter'] = 'Duplicando actividades ({$a->current}/{$a->size})';
-$string['progress_full'] = 'Duplicando el tema';
-$string['error_nosectioninfo'] = 'El tema indicado no contiene información.';
-
-$string['level'] = 'Nivel';
-$string['index'] = 'Inicio';
-$string['asprincipal'] = 'Normal, como una pestaña de primer nivel';
 $string['aschild'] = 'Hijo de la pestaña anterior';
-$string['level_help'] = 'Cambiar el nivel de la pestaña, para aparecer como un subnivel de pestañas.';
-$string['fontcolor'] = 'Color de fuente';
-$string['fontcolor_help'] = 'Utilizado para cambiar el color de la fuente en el nombre de la pestaña. El valor puede ser un color en cualquier representación válida para CSS como por ejemplo: <ul><li>Hexadecimal: #ffffff</li><li>RGB: rgb(0,255,0)</li><li>Nombre: green</li></ul>';
+$string['asprincipal'] = 'Normal, como una pestaña de primer nivel';
 $string['bgcolor'] = 'Color de fondo';
 $string['bgcolor_help'] = 'Utilizado para cambiar el color de fondo del texto en el nombre de la pestaña. El valor puede ser un color en cualquier representación válida para CSS como por ejemplo: <ul><li>Hexadecimal: #ffffff</li><li>RGB: rgb(0,255,0)</li><li>Nombre: green</li></ul>';
-$string['cssstyles'] = 'Propiedades CSS';
-$string['cssstyles_help'] = 'Sirve para cambiar las propiedades CSS de la pestaña. Utilice el formato tradicional del atributo <em>style</em> de una etiqueta html. Ejemplo: <br /><strong>font-weight: bold; font-size: 16px;</strong>';
-$string['firsttabtext'] = 'Nombre de la primera pestaña (inicio) en el subnivel';
-$string['firsttabtext_help'] = 'Si la pestaña tiene un subnivel de pestañas, éste será el texto para la primera pestaña del subnivel.';
-
+$string['cantcreatesection'] = 'Error creando un nuevo tema';
 $string['coursedisplay'] = 'Modo de visualización de la sección 0';
 $string['coursedisplay_help'] = 'Define como se muestra la sección 0: como la primera pestaña o como una sección encima de las demás pestañas.';
-$string['coursedisplay_single'] = 'Como pestaña';
 $string['coursedisplay_multi'] = 'Arriba de las pestañas';
-
-$string['templatetopic'] = 'Usar resumen de tema como plantilla';
-$string['templatetopic_help'] = 'Permite usar el resumen del tema como una plantilla, de esa manera se pueden ubicar los recursos en cualquier parte del contenido, no necesariamente como listas secuenciales como se muestran tradicionalmente en Moodle. <br />Para ubicar un recurso, simplemente agrege en el resumen de la sección el nombre del recurso encerrado entre dobles corchetes, ejemplo: <strong>[[Foro de Novedades]]</strong>. Su comportamiento es similar al filtro por nombre de actividad con la diferencia de que se puede agregar el icono del recurso y además se puede elegir cuales recursos se muestran y cuales no.';
-$string['templetetopic_not'] = 'No, mostrar normal';
-$string['templetetopic_single'] = 'Si, usar el resumen como una plantilla';
-$string['templetetopic_list'] = 'Si, usar el resumen como plantilla y listar los recursos no referenciados';
-$string['templatetopic_icons'] = 'Mostrar icono en enlaces de recursos en el resumen';
-$string['templatetopic_icons_help'] = 'Esta opción define si se muestran o no los iconos de los recursos como parte del nombre, cuando el resumen del tema se utiliza como plantilla.';
+$string['coursedisplay_single'] = 'Como pestaña';
+$string['creating_section'] = 'Creando el nuevo tema';
+$string['cssstyles'] = 'Propiedades CSS';
+$string['cssstyles_help'] = 'Sirve para cambiar las propiedades CSS de la pestaña. Utilice el formato tradicional del atributo <em>style</em> de una etiqueta html. Ejemplo: <br /><strong>font-weight: bold; font-size: 16px;</strong>';
+$string['currentsection'] = 'Este tema';
+$string['defaultsectionsnavigation'] = 'Valor por defecto para la navegación de secciones';
+$string['defaultsectionsnavigation_help'] = 'Valor por defecto a ser utilizado para definir el comportamiento de la funcionalidad "Usar navegación en secciones". Este valor puede ser sobreescrito por cada curso.';
+$string['disable'] = 'Deshabilitar';
+$string['disableajax'] = 'Acciones de edición asíncronas';
+$string['disableajax_help'] = 'Deshabilitarlas le permite mover recursos entre pestañas de temas. Sólo se deshabilitan las acciones asíncronas en la sesión actual, no es permanente.';
+$string['duplicate'] = 'Duplicar';
+$string['duplicate_confirm'] = '¿Está seguro de que desea duplicar el tema actual? La tarea puede tardar un buen rato dependiendo de la cantidad de recursos.';
+$string['duplicatesection'] = 'Duplicar tema actual';
+$string['duplicatesection_help'] = 'Usado para duplicar los recursos del tema actual en un nuevo tema.';
+$string['duplicating'] = 'Duplicando';
+$string['enable'] = 'Habilitar';
+$string['enableanchorposition'] = 'Habilitar ancla de posición';
+$string['enableanchorposition_help'] = 'Usar una ancla para posicionarse en la parte superior de las pestañas cuando se haga clic en una pestaña.';
+$string['enablecustomstyles'] = 'Habilitar estilos personalizados';
+$string['enablecustomstyles_help'] = 'Habilitar color de fuente, color de fondo y otros estilos personalizados de pestañas en la configuración de la sección.';
+$string['error_nosectioninfo'] = 'El tema indicado no contiene información.';
+$string['firsttabtext'] = 'Nombre de la primera pestaña (inicio) en el subnivel';
+$string['firsttabtext_help'] = 'Si la pestaña tiene un subnivel de pestañas, éste será el texto para la primera pestaña del subnivel.';
+$string['fontcolor'] = 'Color de fuente';
+$string['fontcolor_help'] = 'Utilizado para cambiar el color de la fuente en el nombre de la pestaña. El valor puede ser un color en cualquier representación válida para CSS como por ejemplo: <ul><li>Hexadecimal: #ffffff</li><li>RGB: rgb(0,255,0)</li><li>Nombre: green</li></ul>';
 $string['hidden_message'] = 'El tema <em>{$a}</em> no está disponible en este momento.';
-$string['privacy:metadata'] = 'El formato Temas en pestañas no almacena datos personales.';
 $string['hiddensectionshelp'] = 'Las secciones ocultas se muestran en forma colapsada con mensaje de disponibilidad';
-
+$string['hiddentabsbar'] = 'Las pestañas están configuradas para estar ocultas. No se verán cuando no esté en modo de edición.';
+$string['hidefromothers'] = 'Ocultar tema';
+$string['hidetabsbar'] = 'Ocultar barra de pestañas';
+$string['hidetabsbar_help'] = 'Oculta la barra de pestañas en la página principal del curso, la navegación se llevará a cabo con la barra de navegación entre temas.';
+$string['increasesections'] = 'Adicionar una nueva sección después de la sección actual';
+$string['index'] = 'Inicio';
+$string['level'] = 'Nivel';
+$string['level_help'] = 'Cambiar el nivel de la pestaña, para aparecer como un subnivel de pestañas.';
+$string['movesectionto'] = 'Mover el tema actual';
+$string['movesectionto_help'] = 'Mover el tema actual <strong>antes</strong> de (para temas a la izquierda del actual) o <strong>después</strong> de (para temas a la derecha del actual) el tema que seleccione';
+$string['page-course-view-topics'] = 'Alguna página principal de curso en formato onetopic';
+$string['page-course-view-topics-x'] = 'Alguna página de curso en formato onetopic';
+$string['pluginname'] = 'Temas en pestañas';
+$string['privacy:metadata'] = 'El formato Temas en pestañas no almacena datos personales.';
+$string['progress_counter'] = 'Duplicando actividades ({$a->current}/{$a->size})';
+$string['progress_full'] = 'Duplicando el tema';
+$string['rebuild_course_cache'] = 'Recreando el caché del curso';
+$string['sectionname'] = 'Tema';
+$string['sectionsnavigation_both'] = 'En la parte superior e inferior de la sección';
+$string['sectionsnavigation_bottom'] = 'Usar en la parte inferior';
+$string['sectionsnavigation_not'] = 'No usar';
+$string['sectionsnavigation_sitelevel'] = 'Usar el valor por defecto del sitio';
+$string['sectionsnavigation_slides'] = 'Similar a un pase de diapositivas';
+$string['sectionsnavigation_support'] = 'Solamente si el tema no soporta la funcionalidad "Índice de curso"';
+$string['showfromothers'] = 'Mostrar tema';
+$string['subtopictoright'] = 'Mover a la derecha como pestaña hija';
 $string['tabsview'] = 'Vista de pestañas';
+$string['tabsview_default'] = 'Por defecto';
 $string['tabsview_help'] = 'Por defecto: es la vista tradicional de pestañas.<br />
 Verticalmente: se muestran las pestañas al lado izquierdo de manera vertical. El contenido queda a la derecha del menú.<br />
 Una sola línea: todas las pestañas son mostradas en una sola línea, con desplazamiento horizontal si es necesario. Es útil sobre todo cuando hay demasiadas pestañas.';
-$string['tabsview_default'] = 'Por defecto';
-$string['tabsview_vertical'] = 'Verticalmente';
 $string['tabsview_oneline'] = 'Una sola línea';
-
-$string['increasesections'] = 'Adicionar una nueva sección después de la sección actual';
-
-$string['hiddentabsbar'] = 'Las pestañas están configuradas para estar ocultas. No se verán cuando no esté en modo de edición.';
-$string['enablecustomstyles'] = 'Habilitar estilos personalizados';
-$string['enablecustomstyles_help'] = 'Habilitar color de fuente, color de fondo y otros estilos personalizados de pestañas en la configuración de la sección.';
-
-$string['usessectionsnavigation'] = 'Usar navegación en secciones';
-$string['usessectionsnavigation_help'] = 'Mostrar un botón para navegar a la sección anterior y a la siguiente.';
-$string['sectionsnavigation_sitelevel'] = 'Usar el valor por defecto del sitio';
-$string['sectionsnavigation_support'] = 'Solamente si el tema no soporta la funcionalidad "Índice de curso"';
-$string['sectionsnavigation_not'] = 'No usar';
-$string['sectionsnavigation_bottom'] = 'Usar en la parte inferior';
-$string['sectionsnavigation_both'] = 'En la parte superior e inferior de la sección';
-$string['sectionsnavigation_slides'] = 'Similar a un pase de diapositivas';
-$string['enableanchorposition'] = 'Habilitar ancla de posición';
-$string['enableanchorposition_help'] = 'Usar una ancla para posicionarse en la parte superior de las pestañas cuando se haga clic en una pestaña.';
-$string['defaultsectionsnavigation'] = 'Valor por defecto para la navegación de secciones';
-$string['defaultsectionsnavigation_help'] = 'Valor por defecto a ser utilizado para definir el comportamiento de la funcionalidad "Usar navegación en secciones". Este valor puede ser sobreescrito por cada curso.';
+$string['tabsview_vertical'] = 'Verticalmente';
+$string['templatetopic'] = 'Usar resumen de tema como plantilla';
+$string['templatetopic_help'] = 'Permite usar el resumen del tema como una plantilla, de esa manera se pueden ubicar los recursos en cualquier parte del contenido, no necesariamente como listas secuenciales como se muestran tradicionalmente en Moodle. <br />Para ubicar un recurso, simplemente agrege en el resumen de la sección el nombre del recurso encerrado entre dobles corchetes, ejemplo: <strong>[[Foro de Novedades]]</strong>. Su comportamiento es similar al filtro por nombre de actividad con la diferencia de que se puede agregar el icono del recurso y además se puede elegir cuales recursos se muestran y cuales no.';
+$string['templatetopic_icons'] = 'Mostrar icono en enlaces de recursos en el resumen';
+$string['templatetopic_icons_help'] = 'Esta opción define si se muestran o no los iconos de los recursos como parte del nombre, cuando el resumen del tema se utiliza como plantilla.';
+$string['templetetopic_list'] = 'Si, usar el resumen como plantilla y listar los recursos no referenciados';
+$string['templetetopic_not'] = 'No, mostrar normal';
+$string['templetetopic_single'] = 'Si, usar el resumen como una plantilla';
 $string['usescourseindex'] = 'Usar índice de curso';
 $string['usescourseindex_help'] = 'Usar la barra lateral conocida como <em>Índice de curso</em> que permite la navegación a través de secciones y recursos.';
+$string['usessectionsnavigation'] = 'Usar navegación en secciones';
+$string['usessectionsnavigation_help'] = 'Mostrar un botón para navegar a la sección anterior y a la siguiente.';
+$string['utilities'] = 'Utilidades de edición de pestañas';
